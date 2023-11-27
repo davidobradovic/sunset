fe81737c756313556a069c16434ee8e255560e87
David Obradovic <davidobradovic@192.168.1.11>
2023-11-27 03:05:10 +0100

    snow

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Snowfall from 'react-snowfall';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <ToastContainer />
+    <Snowfall style={{ minHeight: '100vh' }} />
   </React.StrictMode>
 );
 
